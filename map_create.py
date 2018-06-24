7b31350d92390515660d481fb38fe6bef083f401
Trevor-Hull <38514059+Trevor-Hull@users.noreply.github.com>
2018-06-23 17:45:34 -0700

    Added files for a population feature
    Added population layer, new world.json file, added layer control, tweaked border line size and width.

--- map_create.py
+++ map_create.py
@@ -16,12 +16,21 @@ def color_producer(elevation):
 
 map_obj = folium.Map(location=[41.034069, -112.234989],zoom_start=5)
 
-fg = folium.FeatureGroup(name='Volcano Map')
+fgp = folium.FeatureGroup(name='Population')
+
+fgp.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read()), style_function=lambda x: {'fillColor':'green' 
+if x['properties']['POP2005']<10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red',
+'color':'grey', 'weight':0.5}, smooth_factor=0.5))
+
+fgv = folium.FeatureGroup(name='Volcano Map')
 
 for lt, lg, el, na, tp in zip(lat,lon,elev,name,vol_type):
-    fg.add_child(folium.CircleMarker(location=[lt,lg], radius= 7, popup=folium.Popup(str(na) + '\n' + str(el) + ' m  Type: ' + str(tp),parse_html=True),
+    fgv.add_child(folium.CircleMarker(location=[lt,lg], radius= 7, popup=folium.Popup(str(na) + '\n' + str(el) + ' m  Type: ' + str(tp),parse_html=True),
     fill_color= color_producer(el), color = 'grey', fill= True, fill_opacity=0.9))
 
-map_obj.add_child(fg)
+map_obj.add_child(fgp)
+map_obj.add_child(fgv)
+
+map_obj.add_child(folium.LayerControl())
 
 map_obj.save('map1.html')
